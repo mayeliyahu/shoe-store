b5b09ecef61d2e6509a294e9906b600cc58fb9a6
Yarin Hershko <yarin5805180@gmail.com>
2024-10-05 10:02:46 +0300

    connect between frontend and backend

--- backend/controllers/shoeController.js
+++ backend/controllers/shoeController.js
@@ -1,54 +1,96 @@
-const Shoe = require('../models/Shoe');
+const { query } = require("express");
+const Shoe = require("../models/Shoe");
 
 exports.getShoes = async (req, res) => {
-    try {
-        const shoes = await Shoe.find();
-        res.json(shoes);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
+  try {
+    const filterQuery = {};
+    const userQuery = req.query;
+
+    if (userQuery.name) {
+      filterQuery.name = userQuery.name;
     }
+    if (userQuery.brand) {
+      filterQuery.brand = userQuery.brand;
+    }
+
+    if (userQuery.size) {
+      filterQuery.availableSizes = { $in: [userQuery.size] };
+    }
+
+    if (userQuery.gender) {
+      filterQuery.gender = { $in: [userQuery.gender, "unisex"] };
+    }
+
+    if (userQuery.minPrice || userQuery.maxPrice) {
+      filterQuery.price = {};
+      if (userQuery.minPrice) {
+        filterQuery.price.$gte = userQuery.minPrice; // Greater than or equal to minPrice
+      }
+      if (userQuery.maxPrice) {
+        filterQuery.price.$lte = userQuery.maxPrice; // Less than or equal to maxPrice
+      }
+    }
+    const shoes = await Shoe.find(filterQuery);
+    res.json(shoes);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 exports.createShoe = async (req, res) => {
-    const { name, brand, availableSizes, inStockSizes, price, inStock, gender } = req.body;
-    try {
-        const newShoe = new Shoe({ name, brand, availableSizes, inStockSizes, price, inStock ,gender });
-        const shoe = await newShoe.save();
-        res.status(201).json(shoe);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+  const { name, brand, availableSizes, inStockSizes, price, inStock, gender } =
+    req.body;
+  try {
+    const newShoe = new Shoe({
+      name,
+      brand,
+      availableSizes,
+      inStockSizes,
+      price,
+      inStock,
+      gender,
+    });
+    const shoe = await newShoe.save();
+    res.status(201).json(shoe);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 exports.getShoeById = async (req, res) => {
-    try {
-        const shoe = await Shoe.findById(req.params.id);
-        if (!shoe) return res.status(404).json({ message: 'Shoe not found' });
-        res.json(shoe);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+  try {
+    const shoe = await Shoe.findById(req.params.id);
+    if (!shoe) return res.status(404).json({ message: "Shoe not found" });
+    res.json(shoe);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 exports.updateShoe = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const updatedShoeData = req.body;
-        const updatedShoe = await Shoe.findByIdAndUpdate(id, updatedShoeData, { new: true, runValidators: true });
-        if (!updatedShoe) return res.status(404).json({ message: 'Shoe not found' });
-        res.json(updatedShoe);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+  try {
+    const { id } = req.params;
+    const updatedShoeData = req.body;
+    const updatedShoe = await Shoe.findByIdAndUpdate(id, updatedShoeData, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updatedShoe)
+      return res.status(404).json({ message: "Shoe not found" });
+    res.json(updatedShoe);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 exports.deleteShoe = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const deletedShoe = await Shoe.findByIdAndDelete(id);
-        if (!deletedShoe) return res.status(404).json({ message: 'Shoe not found' });
-        res.json({ message: 'Shoe deleted' });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+  try {
+    const { id } = req.params;
+    const deletedShoe = await Shoe.findByIdAndDelete(id);
+    if (!deletedShoe)
+      return res.status(404).json({ message: "Shoe not found" });
+    res.json({ message: "Shoe deleted" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,23 +1,49 @@
-const User = require('../models/User');
+const User = require("../models/User");
 
 exports.getUsers = async (req, res) => {
-    try {
-        const users = await User.find();
-        res.json(users);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+  const { name, email, password, isAdmin } = req.query;
+
+  // Build the filter object dynamically
+  const filter = {};
+
+  if (name) filter.name = name;
+  if (email) filter.email = email;
+  if (password) filter.password = password;
+  filter.isAdmin = isAdmin ? isAdmin === "true" : false;
+
+  try {
+    const users = await User.find(filter);
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 exports.createUser = async (req, res) => {
-    const { name, email, password, isAdmin } = req.body;
-    try {
-        const newUser = new User({ name, email, password, isAdmin });
-        const user = await newUser.save();
-        console.log("hereeee")
-        console.log({user})
-        res.status(201).json(user);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+  const { name, email, password, isAdmin } = req.body;
+  try {
+    const newUser = new User({ name, email, password, isAdmin });
+    const user = await newUser.save();
+    console.log("hereeee");
+    console.log({ user });
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  filter = { email: email, password: password };
+  try {
+    if (!email || !password) throw new Error("insert email and pswword");
+
+    const users = await User.find(filter);
+    if (users.length == 0)
+      res.status(404).json({ error: "email or password are incorrect" });
+    res.json(users[0]);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
