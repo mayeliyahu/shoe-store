cc68eec36d43728a080295cb74410e699deb42a7
yael201062 <yael201062@gmail.com>
2024-10-06 21:54:20 +0300

    Search history popup

--- frontend/scripts/scripts.js
+++ frontend/scripts/scripts.js
@@ -1,20 +1,19 @@
-//Searchbox function
+//serach function
 const searchIcon = document.getElementById('search-icon');
 const searchBox = document.getElementById('search-box');
 const searchInput = document.getElementById('search-input');
-const searchDropdown = document.querySelector('.search-dropdown');
-
+const searchHistoryPopup = document.getElementById('search-history-popup');
 searchIcon.addEventListener('click', function (event) {
   event.preventDefault();
-  searchDropdown.classList.add('search-active');
+  searchBox.style.display = 'block'; 
   searchInput.focus();
 });
-
 document.addEventListener('click', function (event) {
-  if (!searchDropdown.contains(event.target)) {
-    searchDropdown.classList.remove('search-active');
+  if (!searchBox.contains(event.target) && !searchIcon.contains(event.target)) {
+    searchBox.style.display = 'none';
   }
 });
+
 
 
 //order summary 
